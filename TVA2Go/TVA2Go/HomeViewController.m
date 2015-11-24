e8de42d1fb9bfd1220cef966c785fb6aa4ae436b
Eyolph <lauracastelli@EnricoMac.local>
2015-11-24 12:24:12 +0100

    set home screen, connected toolbar buttons

--- TVA2Go/TVA2Go/HomeViewController.m
+++ TVA2Go/TVA2Go/HomeViewController.m
@@ -9,6 +9,8 @@
 #import "HomeViewController.h"
 #import <Parse/Parse.h>
 #import <ParseUI/ParseUI.h>
+#import "PinnedViewController.h"
+#import "MostPinnedTableViewController.h"
 
 
 @interface HomeViewController () <UINavigationControllerDelegate>
@@ -18,14 +20,36 @@
 @property (weak, nonatomic) IBOutlet UIButton *smartButton;
 @property (weak, nonatomic) IBOutlet UIButton *randomButton;
 
+
 @end
 
 @implementation HomeViewController
 
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    self.navigationController.navigationBarHidden = YES;
+    self.navigationController.toolbarHidden = NO;
+    
+    UIBarButtonItem *space = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    UIBarButtonItem *myPins = [[UIBarButtonItem alloc] initWithTitle:@"My Pins" style:UIBarButtonItemStylePlain target:self action:@selector(myPins)];
+
+        UIBarButtonItem *login = [[UIBarButtonItem alloc] initWithTitle:@"Login" style:UIBarButtonItemStylePlain target:self action:@selector(login)];
+        UIBarButtonItem *mostPinned = [[UIBarButtonItem alloc] initWithTitle:@"Most Pinned" style:UIBarButtonItemStylePlain target:self action:@selector(mostPinned)];
+
+    
+    self.toolbarItems = @[myPins, space, login, space, mostPinned];
+
+    
+}
+
 - (void)viewDidLoad {
     
     [super viewDidLoad];
-    self.navigationController.navigationBarHidden = YES;
+
+    
 }
 
 
@@ -42,6 +66,45 @@
 - (IBAction)random:(id)sender {
 }
 
+- (void) myPins
+{
+    PinnedViewController *p = [[PinnedViewController alloc] init];
+    [self.navigationController pushViewController:p animated:YES];
+}
+
+- (void) login
+{
+    
+}
+
+- (void) mostPinned
+{
+    MostPinnedTableViewController *m = [[MostPinnedTableViewController alloc] init];
+        [self.navigationController pushViewController:m animated:YES];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    self.navigationController.navigationBarHidden = NO;
+    self.navigationController.toolbarHidden = YES;
+    
+}
+
+
+
+
+
+
+
+
 
 
 @end
+
+
+
+
+
+
