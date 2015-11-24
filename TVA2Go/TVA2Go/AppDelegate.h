b0d144a0aeef5bd6275c0d7b2e45f9f7803c68a7
Eyolph <lauracastelli@EnricoMac.local>
2015-11-24 10:18:47 +0100

    first commit: setup the project

---
+++ TVA2Go/TVA2Go/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TVA2Go
+//
+//  Created by Eyolph on 24/11/15.
+//  Copyright © 2015 Eyolph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
