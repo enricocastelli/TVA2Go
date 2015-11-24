61b650bafb5478059dea5fc28767acf2a5868fa5
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2015-11-24 11:41:33 +0100

    Added client key and application idea. Imported Parse.h to AppDelegate.m.

--- TVA2Go/TVA2Go/AppDelegate.m
+++ TVA2Go/TVA2Go/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#import <Parse/Parse.h>
+
 @interface AppDelegate ()
 
 @end
@@ -16,6 +18,10 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    [Parse setApplicationId:@"257QSfnDrmGSk5BCgBIMJO3tzlwNlABkjZBaRT9K"
+                  clientKey:@"RjnuOLeRozXsx2fzVXYo5Xu2YTslu6GrF8VRpFyw"];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
