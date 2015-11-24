61d2a4f1df1299b3903e03c3d16e949d8ea94af6
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2015-11-24 15:32:22 +0100

    Registered delegate for Table View.

--- TVA2Go/TVA2Go/MostPinnedTableViewController.m
+++ TVA2Go/TVA2Go/MostPinnedTableViewController.m
@@ -17,11 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+ [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
