a1c571b70bdc8ef9b077d939c4169599fa1c2c2e
Eyolph <lauracastelli@EnricoMac.local>
2015-11-24 15:35:34 +0100

    created pin collection cell

---
+++ TVA2Go/TVA2Go/PinCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PinCollectionViewCell.h
+//  TVA2Go
+//
+//  Created by Eyolph on 24/11/15.
+//  Copyright © 2015 Eyolph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PinCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ TVA2Go/TVA2Go/PinCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  PinCollectionViewCell.m
+//  TVA2Go
+//
+//  Created by Eyolph on 24/11/15.
+//  Copyright © 2015 Eyolph. All rights reserved.
+//
+
+#import "PinCollectionViewCell.h"
+
+@implementation PinCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
