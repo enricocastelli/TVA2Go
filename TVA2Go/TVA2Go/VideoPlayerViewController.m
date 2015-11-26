493bd9c5b27e730ae1e02b7a74f0134177d2f2b8
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2015-11-26 17:56:43 +0100

    Got videoID to add to user's array after pushing the heart button.

--- TVA2Go/TVA2Go/VideoPlayerViewController.m
+++ TVA2Go/TVA2Go/VideoPlayerViewController.m
@@ -57,13 +57,23 @@
     
     self.videoObject[@"pinCount"] = newPinCount;
     
+//    PFUser *user = [PFUser currentUser];
+//    [user[@"pinnedVideos"] addObject:self.videoObject[@"videoID"]
+    
     [self.videoObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
         [UIView animateWithDuration:2 delay:0 usingSpringWithDamping:1 initialSpringVelocity:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
             self.likeButton.alpha = 0;
         } completion:nil];
+        PFUser *user = [PFUser currentUser];
+        NSMutableArray *userMustableArray = [user[@"pinnedVideos"] mutableCopy];
+        [userMustableArray addObject:self.videoObject[@"videoID"]];
+        [user setObject:[userMustableArray copy] forKey:@"pinnedVideos"];
+        [user saveInBackground];
+        
+        
     }];
    
-//    self.videoObject[@"sh"]
+   
     
 }
 //
