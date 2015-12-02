c0aa15475d942c0df63420b4d0db164169a97667
Eyolph <lauracastelli@EnricoMac.local>
2015-12-02 17:11:44 +0100

    making animations

---
+++ TVA2Go/TVA2Go/LoadingEffect.h
@@ -0,0 +1,18 @@
+//
+//  LoadingEffect.h
+//  TVA2Go
+//
+//  Created by Eyolph on 02/12/15.
+//  Copyright © 2015 Eyolph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadingEffect : UIView
+
+
+- (void)animateCircle;
+
+@end
+
+
